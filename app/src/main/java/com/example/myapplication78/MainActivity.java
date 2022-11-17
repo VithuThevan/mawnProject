8df7f933ffb5255a7565dbabd0041d493e778569
Vithursan1003 <vithursan1003@gmail.com>
2022-11-17 23:04:26 +0530

    Added some comments

--- app/src/main/java/com/example/myapplication78/MainActivity.java
+++ app/src/main/java/com/example/myapplication78/MainActivity.java
@@ -8,15 +8,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
     EditText name, description, price;
-    Button insert, update, delete, view, add;
+    Button insert, update, delete, view;
+    FloatingActionButton add;
     DBHelper DB;
     boolean isAllFieldsChecked = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,34 +34,37 @@ public class MainActivity extends AppCompatActivity {
         update = findViewById(R.id.btnupdate);
         delete = findViewById(R.id.btndelete);
         view = findViewById(R.id.btnview);
-
+        add = (FloatingActionButton) findViewById(R.id.fab);
 
         DB = new DBHelper(this);
 
+        //Calling addTextChangedListener to limit the decimal values to two
         price.addTextChangedListener(onTextChangedListener());
 
         //to redirect page to list view upon clicking view and update button
         update.setOnClickListener(view -> {
-            boolean isUpdate = DB.updateData(name.getText().toString(),description.getText().toString(),price.getText().toString());
-            if (isUpdate){
+            boolean isUpdate = DB.updateData(name.getText().toString(), description.getText().toString(), price.getText().toString());
+            if (isUpdate) {
                 Toast.makeText(MainActivity.this, "Entry Updated", Toast.LENGTH_SHORT).show();
-            }else{
+            } else {
                 Toast.makeText(MainActivity.this, "Entry not Updated", Toast.LENGTH_SHORT).show();
             }
         });
 
+        //redirect to view page to see all the records updated
         view.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, userlist.class)));
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
+        //to show an alert message before deleting
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
         delete.setOnClickListener(view -> {
             builder.setTitle("Warning").setMessage("Do you want to delete this Item? This action cannot be undone").setCancelable(false)
                     .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
                             Integer deletedRows = DB.deletedata(name.getText().toString());
-                            if (deletedRows>0){
+                            if (deletedRows > 0) {
                                 Toast.makeText(MainActivity.this, "Entry Deleted", Toast.LENGTH_SHORT).show();
-                            }else{
+                            } else {
                                 Toast.makeText(MainActivity.this, "Entry not Deleted", Toast.LENGTH_SHORT).show();
                             }
                         }
@@ -69,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
                     }).show();
         });
 
+        //adding details to database
         insert.setOnClickListener(view -> {
             String nameTXT = name.getText().toString();
             String descriptionTXT = description.getText().toString();
@@ -77,34 +86,31 @@ public class MainActivity extends AppCompatActivity {
             isAllFieldsChecked = CheckAllFields();
 
             Boolean checkinsertdata = DB.insertuserdata(nameTXT, descriptionTXT, priceTXT);
-            if (checkinsertdata){
+            if (checkinsertdata) {
                 Toast.makeText(MainActivity.this, "New Entry Inserted", Toast.LENGTH_SHORT).show();
-            }else{
+            } else {
                 Toast.makeText(MainActivity.this, "New Entry Not Inserted", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
+    //onTextChangedListener which limits the digits beyond decimal points to 2
     private TextWatcher onTextChangedListener() {
         return new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
 
             @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
 
             @Override
             public void afterTextChanged(Editable arg0) {
                 String str = price.getText().toString();
                 if (str.isEmpty()) return;
 
-                String str2 = PerfectDecimal(str,2);
+                String str2 = PerfectDecimal(str, 2);
 
-                if (!str2.equals(str)){
+                if (!str2.equals(str)) {
                     price.setText(str2);
                     price.setSelection(str2.length());
                 }
@@ -113,29 +119,32 @@ public class MainActivity extends AppCompatActivity {
         };
     }
 
-    public String PerfectDecimal(String str, int maxdecimal){
-        if(str.charAt(0)=='.') str = "0"+str;
+    public String PerfectDecimal(String str, int maxdecimal) {
+        if (str.charAt(0) == '.') str = "0" + str;
         int max = str.length();
 
         StringBuilder rFinal = new StringBuilder();
         boolean after = false;
-        int i=0, decimal=0; char t;
-        while(i<max){
-            t=str.charAt(i);
-            if(t!='.' && after==false){
-            }else if(t=='.'){
-                after=true;
-            }else{
+        int i = 0, decimal = 0;
+        char t;
+        while (i < max) {
+            t = str.charAt(i);
+            if (t != '.' && after == false) {
+            } else if (t == '.') {
+                after = true;
+            } else {
                 decimal++;
-                if(decimal>maxdecimal){
+                if (decimal > maxdecimal) {
                     return rFinal.toString();
                 }
             }
             rFinal.append(t);
             i++;
-        }return rFinal.toString();
+        }
+        return rFinal.toString();
     }
 
+    //validation to check whether all fields are entered before inserting
     private boolean CheckAllFields() {
         if (name.length() == 0) {
             name.setError("This field is required");
--- app/src/main/java/com/example/myapplication78/userlist.java
+++ app/src/main/java/com/example/myapplication78/userlist.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -25,29 +26,38 @@ public class userlist extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userlist);
+
         DB = new DBHelper(this);
+
         Name = new ArrayList<>();
         Description = new ArrayList<>();
         Price = new ArrayList<>();
-        recyclerView = findViewById(R.id.recyclerview);
         add = (FloatingActionButton) findViewById(R.id.fab);
+
+        recyclerView = findViewById(R.id.recyclerview);
         adapter = new MyAdapter(this,Name,Description,Price);
         recyclerView.setAdapter(adapter);
+
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         displaydata();
 
-        add.setOnClickListener(add -> startActivity(new Intent(userlist.this, MainActivity.class)));
+        //hiding the update and view buttons and changing it to fill the form
+        add.setOnClickListener(view -> {
+            startActivity(new Intent(userlist.this, MainActivity.class));
+            MainActivity Main = new MainActivity();
+                Main.update.setVisibility(View.INVISIBLE);
+                Main.delete.setVisibility(View.INVISIBLE);
+        });
     }
 
-
+    //the method to show all the details in card
     private void displaydata() {
         Cursor cursor = DB.getdata();
         if (cursor.getCount()==0){
             Toast.makeText(userlist.this, "No Entry Exists", Toast.LENGTH_SHORT).show();
-
-//        }else{
-//            Name.add(cursor.getString(0));
-//            Description.add(cursor.getString(1));
-//            Price.add(cursor.getString(2));
+        }else{
+            Name.add(cursor.getString(0));
+            Description.add(cursor.getString(1));
+            Price.add(cursor.getString(2));
     }
 }}
